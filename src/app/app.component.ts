5fa77723bfecba9a96d049c05c9fb8f3f0bc94f4
Jofred <reinosaj@gmail.com>
2024-08-08 17:12:02 -0400

    mat table dynamic columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface User {
+  id: string;
+  dob: string;
+  country: string;
+  postcode: string;
+  userName: string;
+  fullName: string;
+  phone: string;
+  registered: string;
+  cellPhone: string;
+  email: string;
+  gender: string;
+}
+
+export interface TableColumns {
+  id: string;
+  name: string;
+  isShown: boolean;
+  isDate: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
   title = 'matTableDynamicColumns';
+  tableColumns: TableColumns[] = [
+    {id: 'id', name: 'Id.', isShown: false, isDate: false},
+    {id: 'fullName', name: 'Nombre completo.', isShown: true, isDate: false},
+    {id: 'userName', name: 'Usuario.', isShown: false, isDate: false},
+    {id: 'email', name: 'Email.', isShown: true, isDate: false},
+    {id: 'dob', name: 'Fecha de nacimiento.', isShown: true, isDate: true},
+    {id: 'country', name: 'País.', isShown: false, isDate: false},
+    {id: 'postcode', name: 'Código postal.', isShown: false, isDate: false},
+    {id: 'phone', name: 'Teléfono.', isShown: false, isDate: false},
+    {id: 'cellPhone', name: 'Celular.', isShown: true, isDate: false},
+    {id: 'registered', name: 'Fecha de registro.', isShown: false, isDate: true},
+    {id: 'gender', name: 'Género.', isShown: false, isDate: false},
+  ];
+
+  displayedColumns: string[] = ['fullName', 'email', 'dob', 'cellPhone'];
+
+  dataSource!: MatTableDataSource<User[]>;
+  showColumnOptions = false;
+
+  ngOnInit(): void {
+    this.http.get('https://randomuser.me/api/?results=10')
+    .pipe( map((data: any) => {
+      const response: User[] = data.results.map((user: any) => {
+        return {
+          id: user.login.uuid.substr(0,5),
+          fullName: `${user.name.first} ${user.name.last}`,
+          dob: user.dob.date,
+          country: user.location.country,
+          postcode: user.location.postcode,
+          userName: user.login.username,
+          phone: user.phone,
+          registered: user.registered.date,
+          cellPhone: user.cell,
+          email: user.email,
+          gender: user.gender
+        }
+      });
+      return response;
+    }))
+    .subscribe((values: any) => this.dataSource = values);
+  }
+
+  setShownColumn(element: any, columnId: string) {
+    element.checked
+    ? this.displayedColumns.push(columnId)
+    : this.displayedColumns = this.displayedColumns.filter(colName => colName !== columnId);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatIconModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
